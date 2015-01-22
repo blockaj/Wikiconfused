48b607fe2f385bf46610228f4ad0cb878e881cc8
Aaron <ajblock@gmail.com>
2015-01-21 22:06:14 -0500

    A lot of changes

--- main.js
+++ main.js
@@ -2,31 +2,33 @@ var request = require('request'),
     cheerio = require('cheerio'),
     _ = require('lodash'),
     VerEx = require('verbal-expressions'),
-    Twitter = require('twitter');
+    Twitter = require('twitter'),
+    express = require('express'),
+    app = express();
+
 
 var client = new Twitter({
     consumer_key: 'vCb2Cf16cyrNt13Wd76LfsXyV',
     consumer_secret: '3fGDJythkSOG29ahBXEyLN4t5Z64GTBg2grK2dpy8WzwUXWyMU',
     access_token_key: '2990681327-V9qAnFXpCiqtpaUF97IwahfMmbpfxHgvRZnq1Dm',
     access_token_secret: 'xusyFmpjZX3Y5OPkqvvIjnFUEKEovNxkkNXAID7Qi8FEI'
-})
+});
 
 parentheses = VerEx().find('(').anything().then(')');
-setTimeout(function(){
+setInterval(function(){
+    console.log('Running...');
     request.get('http://en.wikipedia.org/wiki/Special:Random', function(err, response, body) {
-        var $, randomPageTitle;
         if (!err && response.statusCode === 200) {
-            $ = cheerio.load(body);
-            randomPageTitle = $('h1#firstHeading').text();
+            var $ = cheerio.load(body);
+            var randomPageTitle = $('h1#firstHeading').text();
             randomPageTitle = randomPageTitle.split(' ').join('_');
             console.log(randomPageTitle);
             return request.get('http://en.wikipedia.org/wiki/Special:Random', function(err, response, body) {
-                var firstPara, pageTitle, withoutParenth;
                 if (!err && response.statusCode === 200) {
                     $ = cheerio.load(body);
-                    firstPara = $('p').text();
-                    pageTitle = $('p b').text();
-                    withoutParenth = firstPara.replace(parentheses, '');
+                    var firstPara = $('p').text();
+                    var pageTitle = $('p b').text();
+                    var withoutParenth = firstPara.replace(parentheses, '');
                     randomPageTitle = randomPageTitle.split('_').join(' ');
                     withoutParenth = withoutParenth.replace(pageTitle, randomPageTitle);
                     for (i = 0; i < withoutParenth.length; i++) {
@@ -48,10 +50,9 @@ setTimeout(function(){
 
 
 wordComparison = function(title, text) {
-  var length, returnValue, wikiless;
-  returnValue = false;
-  length = title.length;
-  wikiless = removeWikiCharacters(text);
+  var returnValue = false;
+  var length = title.length;
+  var wikiless = removeWikiCharacters(text);
   console.log(text);
   return returnValue = wikiless.indexOf(title);
 };
